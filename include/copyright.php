2ebc114ef42f63261a1cd998ec2457dda3e93c31
Egor <woornir@yandex.ru>
2025-03-09 22:42:27 +0300

    fix tas + 5

--- include/copyright.php
+++ include/copyright.php
@@ -1,11 +1,2 @@
 <p>
-	<?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_SHOW" => "page",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => ""
-	)
-);?>
 </p>
